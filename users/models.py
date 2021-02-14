fdc9fb96349740e3550e18df50778dece9837144
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2021-02-15 02:57:59 +0330

    Add __str__ representation for user model

--- users/models.py
+++ users/models.py
@@ -6,3 +6,6 @@ class Member(AbstractUser):
     first_name = models.CharField(max_length=150, blank=False)
     last_name = models.CharField(max_length=150, blank=False)
     email = models.EmailField(blank=False, unique=True)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
