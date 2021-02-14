0582ceebd964887dfa99d251d3d8676db8b7ebe2
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2021-02-15 02:29:40 +0330

    Remove unwanted tabs in auth serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -13,13 +13,13 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password': {'write_only': True},
         }
 
-        def create(self, validated_data):
-            member = Member.objects.create_user(username=validated_data['username'],
-                                                password=validated_data['password'],
-                                                first_name=validated_data['first_name'],
-                                                last_name=validated_data['last_name'],
-                                                email=validated_data['email'])
-            return member
+    def create(self, validated_data):
+        member = Member.objects.create_user(username=validated_data['username'],
+                                            password=validated_data['password'],
+                                            first_name=validated_data['first_name'],
+                                            last_name=validated_data['last_name'],
+                                            email=validated_data['email'])
+        return member
 
 
 class MemberSerializer(serializers.ModelSerializer):
