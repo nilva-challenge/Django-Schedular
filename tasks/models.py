9452a8509481e99399e13b275d7bcb222b16e0e6
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2021-02-16 21:14:24 +0330

    Add task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=150, blank=False)
+    description = models.CharField(max_length=300, blank=True)
+    owner = models.ForeignKey('users.Member', on_delete=models.CASCADE, related_name='Tasks')
+    time_to_send = models.DateTimeField(blank=False)
+
+    def __str__(self):
+        return self.title
