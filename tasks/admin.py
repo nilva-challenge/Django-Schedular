df78c15987940bdc8d97fa51669bb8a94117a76b
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2021-02-16 22:52:30 +0330

    Create a new interface for non-staff users

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,4 +1,9 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from .models import Task
 
@@ -10,3 +15,33 @@ class MemberAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description', 'owner')
     list_filter = ('owner', 'time_to_send')
     list_per_page = 20
+
+
+class UserAuthenticationForm(AuthenticationForm):
+    error_messages = {
+        **AuthenticationForm.error_messages,
+        'invalid_login': _(
+            "Please enter the correct %(username)s and password for a user "
+            "account. Note that both fields may be case-sensitive."
+        ),
+    }
+    required_css_class = 'required'
+
+    def confirm_login_allowed(self, user):
+        super().confirm_login_allowed(user)
+        if user.is_staff:
+            raise ValidationError(
+                self.error_messages['invalid_login'],
+                code='invalid_login',
+                params={'username': self.username_field.verbose_name}
+            )
+
+
+class UserSite(AdminSite):
+    login_form = UserAuthenticationForm
+
+    def has_permission(self, request):
+        return request.user.is_active and not request.user.is_staff
+
+
+user_site = UserSite(name='user_interface')
