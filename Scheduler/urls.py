3a93e362124ec403bcc56baf4460b16e6fd39e6d
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2021-02-16 22:12:40 +0330

    Add tasks urls to base urls

--- Scheduler/urls.py
+++ Scheduler/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1.0/auth/', include('authentication.urls')),
+    path('api/v1.0/task/', include('tasks.urls')),
 ]
