6c8e0af22cfdff9eb6c62df0b439fcff3e981f46
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2021-02-16 22:08:16 +0330

    Add an API for users and admin to get a list of tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        qs = Task.objects.all()
+        if not user.is_staff:
+            qs = Task.objects.filter(owner=user)
