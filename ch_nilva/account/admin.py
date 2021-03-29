4d80a96a4ee98fb18c149098b305edc253682a28
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-03-29 15:18:46 +0430

    customize admin_panel for manage accounts

--- ch_nilva/account/admin.py
+++ ch_nilva/account/admin.py
@@ -1,4 +1,50 @@
 from django.contrib import admin
 from .models import User
+from django import forms
+from schedule.models import Task
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomAccountAdmin(admin.ModelAdmin):
+    list_display = ('username', 'get_fullname', 'is_admin', 'is_superuser',)
+    list_filter = ('username', 'email', 'is_admin',)
+
+    def get_fullname(self, obj):
+        return obj.first_name + ' ' + obj.last_name
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser or request.user.is_admin
+
+    def has_module_permission(self, request):
+        if request.user.is_anonymous:
+            return False
+        return True
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_admin or request.user.is_superuser
+
+    def get_queryset(self, request):
+        qs = super(CustomAccountAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        if request.user.is_admin:
+            qs = qs.filter(is_superuser=False)
+            return qs
+        return qs.filter(username=request.user.username)
+
+    def add_view(self, request, form_url='', extra_context=None):
+        if request.user.is_superuser:
+            self.exclude = ()
+        elif request.user.is_admin:
+            self.exclude = ('is_superuser', 'user_permissions', 'groups', 'is_staff', 'is_active')
+        return super(CustomAccountAdmin, self).add_view(request, form_url, extra_context)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        if request.user.is_superuser:
+            self.exclude = ()
+        elif request.user.is_admin:
+            self.exclude = ('is_superuser', 'user_permissions', 'groups', 'is_staff', 'is_active')
+        return super(CustomAccountAdmin, self).change_view(request, object_id, form_url, extra_context)
